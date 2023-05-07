4456029113d242b9a504657c98f75ed3e061217a
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2023-05-07 02:31:35 +0200

    Add more predicates

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 #include "trimString.h"
+#include "Predicate.h"
 
 int main() {
     std::string text = "      123 example      ";
     std::cout << text << std::endl;
-    trimString(text, isspace);
+    trimString(text, isWhitespace);
     std::cout << text << std::endl;
     return 0;
 }
