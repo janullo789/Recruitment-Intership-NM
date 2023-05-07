6b854c1ba437cbeac51b1c4cde214be1f335f030
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2023-05-08 00:07:05 +0200

    Add more tests

--- Task3/tests/test.cpp
+++ Task3/tests/test.cpp
@@ -20,3 +20,40 @@ TEST_CASE("testing the isWhitespace predicate with function") {
     CHECK(trimString(cText, isWhitespace) == "Text in char []");
 }
 
+TEST_CASE("testing the isDigit predicate with function") {
+    std::string text = "2Hello World!987  55";
+    CHECK(trimString(text, isDigit) == "Hello World!987  ");
+
+    text = "123A";
+    CHECK(trimString(text, isDigit) == "A");
+
+    text = "ABC123ABC";
+    CHECK(trimString(text, isDigit) == "ABC123ABC");
+}
+
+TEST_CASE("testing the isAlpha predicate with function") {
+    std::string text = "Hello World!";
+    CHECK(trimString(text, isAlpha) == " World!");
+
+    text = "    Hello World!  ";
+    CHECK(trimString(text, isAlpha) == "    Hello World!  ");
+
+    text = "ABC123ABC";
+    CHECK(trimString(text, isAlpha) == "123");
+}
+
+TEST_CASE("testing the isUpperCase predicate with function") {
+    std::string text = "Hello World!";
+    CHECK(trimString(text, isUpperCase) == "ello World!");
+
+    text = "AaBbCc";
+    CHECK(trimString(text, isUpperCase) == "aBbCc");
+}
+
+TEST_CASE("testing the isLowerCase predicate with function") {
+    std::string text = "Hello World";
+    CHECK(trimString(text, isLowerCase) == "Hello W");
+
+    text = "aaaBBBccc";
+    CHECK(trimString(text, isLowerCase) == "BBB");
+}
